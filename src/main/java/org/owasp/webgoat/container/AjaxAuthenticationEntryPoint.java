2c20a414225e54485d85454bd490e67a5f933950
bogdanganzha <121808191+bogdanganzha@users.noreply.github.com>
2023-05-11 09:33:58 +0200

    Update AjaxAuthenticationEntryPoint.java

--- src/main/java/org/owasp/webgoat/container/AjaxAuthenticationEntryPoint.java
+++ src/main/java/org/owasp/webgoat/container/AjaxAuthenticationEntryPoint.java
@@ -44,6 +44,7 @@ public class AjaxAuthenticationEntryPoint extends LoginUrlAuthenticationEntryPoi
     super(loginFormUrl);
   }
 
+  
   @Override
   public void commence(
       HttpServletRequest request,
